7c829b5c0da6a5e1ea7209893127cd03a1bbf7d8
mhmad-jamal <mohammad52008@hotmail.com>
2023-08-03 14:24:50 +0300

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,29 @@
+<?php
+
+/* URL PROJECT */
+
+define('SITE_URL', 'https://react.wetherejo.com/');
+
+/* DATABASE CONFIGURATION */
+
+$database = array(
+    'host' => 'localhost',
+    'db' => 'fit',
+    'user' => 'root',
+    'pass' => ''
+);
+
+$email_config = array(
+    'email_address' => 'EMAIL_ADDRESS_HERE',
+    'email_password' => 'PASSWORD_HERE',
+    'email_subject' => 'EMAIL_SUBJECT_HERE',
+    'email_name' => 'EMAIL_NAME_HERE',
+    'smtp_host' => 'EMAIL_HOST_HERE',
+    'smtp_port' => 'EMAIL_PORT_HERE',
+    'smtp_encrypt' => 'tls'
+);
+
+$items_config = array(
+    'items_per_page' => '8',
+    'images_folder' => 'images/'
+);
