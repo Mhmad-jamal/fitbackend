51a0c620618b6dd611440c9260729077bf4ca37c
mhmad-jamal <mohammad52008@hotmail.com>
2023-08-28 13:35:00 +0300

    sss

--- controller/addSupport.php
+++ controller/addSupport.php
@@ -9,39 +9,48 @@ session_start();
         header ('Location: ' . SITE_URL . '/controller/error.php');
     }
     $response= array();
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['user_id']) && isset($_POST['message']) ) {
-        $user_id = cleardata($_POST['user_id']);
-        $message = cleardata($_POST['message']);
-        
-        $statement = $connect->prepare(
-            'INSERT INTO support (user_id, message) VALUES (:user_id, :message)'
-        );
-        
-        $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-        $statement->bindParam(':message', $message, PDO::PARAM_STR);
-        
-        $statement->execute();
-        
-        
-    
-   
-        if ($statement) {
-             $response=[
-                "success" => true,
-                "message" => 'Message sent successfully',
-                "status" => 200
-        ];
-
-        
+    try {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['user_id']) && isset($_POST['message'])) {
+            $user_id = cleardata($_POST['user_id']);
+            $message = cleardata($_POST['message']);
+            
+            $statement = $connect->prepare(
+                'INSERT INTO support (user_id, message) VALUES (:user_id, :message)'
+            );
+            
+            $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+            $statement->bindParam(':message', $message, PDO::PARAM_STR);
+            
+            $statement->execute();
+            
+            if ($statement) {
+                $response = [
+                    "success" => true,
+                    "message" => 'Message sent successfully',
+                    "status" => 200
+                ];
+            }
+        } else {
+            $response = [
+                "success" => false,
+                "message" => 'Fill required data',
+                "status" => 201
+            ];
         }
-    }
-    else{
-         $response=[
+    } catch (PDOException $e) {
+        // Print the exception message for debugging
+        echo "Exception: " . $e->getMessage();
+    
+        // Handle the exception here
+        // You can log the error, generate a proper error response, etc.
+        $response = [
             "success" => false,
-            "message" => 'Fill required data',
-            "status" => 201
-    ];
+            "message" => 'An error occurred while processing your request',
+            "status" => 500
+        ];
     }
     
+    header('Content-Type: application/json');
     echo json_encode($response);
+    
 ?>
