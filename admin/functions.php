5dc94ddf371c9754ff6a71ea9b9d3f752a4ba993
mhmad-jamal <mohammad52008@hotmail.com>
2023-08-28 13:06:33 +0300

    update

--- admin/functions.php
+++ admin/functions.php
@@ -394,44 +394,25 @@ function get_workouts_by_goal($connect)
         $sentence->execute();
 
 
-        if ($sentence->rowCount() > 0) {
+        $last_record = $sentence->fetch(PDO::FETCH_ASSOC);
 
-            $sentence = $connect->prepare("SELECT * FROM `usesr_goal_workout` WHERE user_id = '$user_id' ORDER BY id DESC LIMIT 1");
-            $sentence->execute();
-
-            if ($sentence->rowCount() > 0) {
-
-                $last_record = $sentence->fetch(PDO::FETCH_ASSOC);
-
-                $timestamp_from_db = strtotime($last_record['created_at']);
-                
-                $today = strtotime(date('Y-m-d'));
-                
-                $seven_days_later = strtotime('+7 days', $today);
-                
-                $workout_id = $last_record["workout_id"];
-                
-                if ($timestamp_from_db < $seven_days_later) {
-                    // Case when created_at is less than 7 days from today
-                    $result = insert_workout($connect, $user_id, $workout_id);
-                    if ($result) {
-                        $result = get_workout_per_id($connect, $workout_id);
-                    }
-                } else {
-                    $result = get_workout_per_id($connect, $workout_id);
-                }
-                
-            } else {
-                $result = insert_workout($connect, $user_id, null);
-                if ($result) {
-                    $result = get_workout_per_id($connect, $result);
-                }
-            }
-            return $result;
+        $timestamp_from_db = strtotime($last_record['created_at']);
+        $today = strtotime(date('Y-m-d'));
+        $seven_days_ago = strtotime('-7 days', $today); // Calculate 7 days ago
+        
+        $workout_id = $last_record["workout_id"];
+        
+        if ($timestamp_from_db >= $seven_days_ago) {
+            // Case when created_at is within the last 7 days
+            $result = get_workout_per_id($connect, $workout_id);
         } else {
-
-            return false;
+            // Case when created_at is more than 7 days ago
+            $result = insert_workout($connect, $user_id, $workout_id);
+            if ($result) {
+                $result = get_workout_per_id($connect, $workout_id);
+            }
         }
+        
     } else {
         return false;
     }
