80c0279d8efa7ea908ecfa8f5e64b5f2bf32eca4
Mhmad-jamal <mohammed52008@hotmail.com>
2023-09-01 19:29:58 +0300

    update

--- controller/need_update.php
+++ controller/need_update.php
@@ -11,16 +11,25 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ( isset($_POST["user_id"] )) {
+if (isset($_POST["user_id"])) {
     $user_id = $_POST["user_id"]; 
-    $user_data = mysqli_query($conn, "SELECT * FROM `users_goal` WHERE `user_id` LIKE '$user_id'");
+
+    // Calculate the date one month ago from today
+    $oneMonthAgo = date("Y-m-d", strtotime("-1 month"));
+
+    $query = "SELECT * FROM `users_goal` WHERE `user_id` = '$user_id' AND `created_at` < DATE_SUB(CURDATE(), INTERVAL 1 MONTH)";
+    
+    $user_data = mysqli_query($conn, $query);
+
     if (mysqli_num_rows($user_data) > 0) {
-
-        $user_data_arr = mysqli_fetch_assoc($user_data);
-var_dump($user_data_arr);
-    }else {
+        while ($row = mysqli_fetch_assoc($user_data)) {
+            // Process the retrieved records here
+            $response["status"] = 201;
+            $response["message"] = " user goals created in the last month!";
+        }
+    } else {
         $response["status"] = 201;
-        $response["message"] = "user not found!";
+        $response["message"] = "No user goals created in the last month!";
     }
-
+    echo json_encode($response);
 }
