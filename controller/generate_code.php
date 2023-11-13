43873ccb72fad98d6e314dffc776243f587dee39
Mhmad-jamal <110853406+Mhmad-jamal@users.noreply.github.com>
2023-11-13 19:51:54 +0300

    sss

--- controller/generate_code.php
+++ controller/generate_code.php
@@ -7,7 +7,9 @@ if (isset($_SESSION['manager_email'])) {
     require '../views/header.view.php';
     require '../views/navbar.view.php';
     $connect = connect($database);
-
+if(isset($_SESSION['insert_message'])){
+    unset($_SESSION['insert_message']);
+}
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && !isset($_POST['id'])) {
         $code = cleardata($_POST['code']);
         $subscription_id = cleardata($_POST["subscription_id"]);
