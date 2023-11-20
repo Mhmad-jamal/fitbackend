60c96b4bb55dc4ebc1214158bce24881c28e84ba
Mhmad-jamal <110853406+Mhmad-jamal@users.noreply.github.com>
2023-11-20 20:40:09 +0300

    sss

--- controller/new_recipe.php
+++ controller/new_recipe.php
@@ -46,6 +46,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$statment = $connect->prepare(
 		'INSERT INTO diets (diet_id,diet_title,diet_description,diet_ingredients,diet_category,diet_directions,diet_calories,diet_carbs,diet_protein,diet_fat,course,diet_time,diet_servings,diet_featured,diet_status,diet_price,diet_image) VALUES (null, :diet_title, :diet_description, :diet_ingredients, :diet_category, :diet_directions, :diet_calories, :diet_carbs, :diet_protein, :diet_fat,:course, :diet_time, :diet_servings, :diet_featured, :diet_status, :diet_price, :diet_image)'
 		);
+		echo "<br>";
+		echo "<br>";
+		echo "<br>";
+		var_dump("Here");
 
 		
 	$result =$statment->execute(array(
