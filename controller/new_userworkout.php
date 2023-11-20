5b399ac807dca052e7baac1997186e1549e7c39b
Mhmad-jamal <110853406+Mhmad-jamal@users.noreply.github.com>
2023-11-20 21:33:08 +0300

    update

--- controller/new_userworkout.php
+++ controller/new_userworkout.php
@@ -1,32 +1,22 @@
 <?php 
-session_start();
 
 session_start();
 if (isset($_SESSION['manager_email'])){
-
+    
     
 require '../admin/config.php';
 require '../admin/functions.php';
 
 $connect = connect($database);
-var_dump("Ssssssssssssssssss");
-
 if(!$connect){
 	header ('Location: ' . SITE_URL . '/controller/error.php');
 	}
-	var_dump($_SERVER['REQUEST_METHOD']);
-
-die(); 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-	 
+
 $user = cleardata($_POST['user_id']);
 $workout = cleardata($_POST['workout_id']);
-var_dump($user);
-var_dump($workout);
-var_dump("@@@@@@@@@");
 
-die();    
 $statment = $connect->prepare("INSERT INTO workouts_users (ws_workout,ws_user) VALUES (:ws_workout, :ws_user)");
 
 $statment->execute(array(
