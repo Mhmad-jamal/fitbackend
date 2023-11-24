bba2d8bbb844852c53bc4506cf900975c2289c9f
Mhmad-jamal <110853406+Mhmad-jamal@users.noreply.github.com>
2023-11-24 18:29:16 +0300

    update

--- admin/functions.php
+++ admin/functions.php
@@ -480,50 +480,109 @@ function insert_Food($connect, $user_id, $prev_id)
         $user_data = $sentence->fetch(PDO::FETCH_ASSOC);
 
         $user_goal = json_decode($user_data["user_goal"]);
-
+$bmi=0;
+$weight=0;
+$Allergies=[];
+$calories=0;
+$food_category=[];
         foreach ($user_goal as $key => $value) {
 
-            if ($value->componentId == 16) {
-                $diet = $value->value;
+            if ($value->componentId == "bmi") {
+                $bmi=$value->value;
+            }
+            if ($value->componentId == "weight") {
+                $weight=$value->value;
+            }
+            if ($value->componentId == "15") {
+                $Allergies=$value->value;
+            } if ($value->componentId == "18") {
+                $food_category=$value->value;
             }
         }
-
-
-
-        if ($diet == '' || $diet == 2) {
+        if($bmi==0 || $weight==0 ){
             return false;
+
         }
+       
+        if ($bmi > 40) {
+            $calories = $weight * 15;
+        } elseif ($bmi <= 40 && $bmi > 30) {
+            $calories = $weight * 20;
+        } elseif ($bmi <= 30 && $bmi > 25) {
+            $calories = $weight * 25;
+        } elseif ($bmi <= 25 && $bmi > 18) {
+            $calories = $weight * 30;
+        } elseif($bmi<18) {
+            $calories = $weight * 50;
+
+        }
+
+        
+        if (count($food_category) == 0 ) {
+           return false;
+
+        }
+
 
 
         $diet_id = '';
+        $minCalories = ((int)$calories - 100);
+        $maxCalories = ((int)$calories + 100);
         if ($prev_id != null) {
-            $sentence = $connect->prepare("SELECT diets.*, categories.category_title AS category_title FROM diets, categories WHERE diet_id != '$prev_id' AND diet_improvement = '$diet' AND diets.diet_category = categories.category_id ORDER BY RAND() LIMIT 1");
 
+            $food_categoryids = implode(",", $food_category);
 
+            $AllergiesJSON = json_encode($Allergies);
+            
+            $sentence = $connect->prepare("SELECT * FROM program_food
+                            WHERE calories BETWEEN $minCalories AND $maxCalories
+                            AND JSON_CONTAINS(Allergies, :userAllergies)
+                            AND category_id IN ($food_categoryids) 
+                            AND id !=$prev_id
+                            ORDER BY RAND() 
+                            LIMIT 1");
+            
+            $sentence->bindParam(':userAllergies', $AllergiesJSON, PDO::PARAM_STR);
             $sentence->execute();
         } else {
-            $sentence = $connect->prepare("SELECT diets.*,categories.category_title AS category_title FROM diets,categories WHERE   diet_improvement='$diet' AND   diets.diet_category = categories.category_id  ORDER BY RAND() limit 1");
+            $food_categoryids = implode(",", $food_category);
+
+            $AllergiesJSON = json_encode($Allergies);
+            
+            $sentence = $connect->prepare("SELECT * FROM program_food
+                            WHERE calories BETWEEN $minCalories AND $maxCalories
+                            AND JSON_CONTAINS(Allergies, :userAllergies)
+                            AND category_id IN ($food_categoryids)
+                            ORDER BY RAND() 
+                            LIMIT 1");
+            
+            $sentence->bindParam(':userAllergies', $AllergiesJSON, PDO::PARAM_STR);
             $sentence->execute();
         }
+       
+        
+        $program = $sentence->fetchAll();
+       
 
-        $diet_data = $sentence->fetchAll();
-        if (count($diet_data) > 0) {
+        
+        if (count($program) > 0) {
 
-            $diet_id = $diet_data[0]['diet_id'];
+            $program_id = $program[0]['id'];
         } else {
             return false;
         }
+       
         $statement = $connect->prepare(
-            'INSERT INTO usesr_goal_diet (user_id, diet_id) VALUES (:user_id, :diet_id)'
+            'INSERT INTO users_goal_diet (user_id, program_id) VALUES (:user_id, :program_id)'
         );
 
         $insertResult = $statement->execute(array(
             ':user_id' => $user_id,
-            ':diet_id' => $diet_id,
+            ':program_id' => $program_id,
         ));
 
         if ($insertResult) {
-            return $diet_id;
+            return $program_id;
         } else {
             return false;
         }
@@ -539,7 +598,7 @@ function get_food_by_goal($connect)
     if ($sentence->rowCount() > 0) {
 
 
-        $sentence = $connect->prepare("SELECT * FROM `usesr_goal_diet` WHERE user_id = '$user_id' ORDER BY id DESC LIMIT 1");
+        $sentence = $connect->prepare("SELECT * FROM `users_goal_diet` WHERE user_id = '$user_id' ORDER BY id DESC LIMIT 1");
         $sentence->execute();
 
         if ($sentence->rowCount() > 0) {
@@ -547,23 +606,26 @@ function get_food_by_goal($connect)
             $timestamp_from_db = strtotime($last_record['created_at']);
 
             $today = strtotime(date('Y-m-d'));
-
+          
             $seven_days_ago = strtotime('-7 days', $today); // Calculate 7 days ago
-            $diet_id = $last_record["diet_id"];
-            if ($timestamp_from_db >= $seven_days_ago) {
+            $program_id = $last_record["program_id"];
+          
 
-                $result = get_diet_per_id($connect, $diet_id);
+            if ($timestamp_from_db > $seven_days_ago) {
+                $result = get_food_program_by_id_mobile($connect, $program_id);
             } else {
-
-                $result = insert_Food($connect, $user_id, $diet_id);
+              
+                $result = insert_Food($connect, $user_id, $program_id);
                 if ($result) {
-                    $result = get_diet_per_id($connect, $diet_id);
+                  
+                    $result = get_food_program_by_id_mobile($connect, $result);
+                    
                 }
             }
         } else {
             $result = insert_Food($connect, $user_id, null);
             if ($result) {
-                $result = get_diet_per_id($connect, $result);
+                $result = get_food_program_by_id_mobile($connect, $result);
             }
         }
         return $result;
@@ -740,8 +802,111 @@ function get_food_program_by_id($connect, $id)
 {
     $sentence = $connect->query("SELECT * FROM program_food  WHERE id =$id LIMIT 1");
     $sentence = $sentence->fetchAll();
+
     return ($sentence) ? $sentence : false;
+
 }
+function get_food_program_by_id_mobile($connect, $id)
+{
+    // Assuming $id is validated and sanitized
+    $query = "SELECT program_food.*, categories.category_title AS category_title, 
+              categories.category_image AS category_image
+              FROM program_food
+              LEFT JOIN categories ON program_food.category_id = categories.category_id
+              WHERE program_food.id = :id
+              LIMIT 1";
+
+    $stmt = $connect->prepare($query);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $program = $stmt->fetch(PDO::FETCH_ASSOC);
+   
+    if($program){
+        
+        $program_food=json_decode($program["program"]);
+$day1_values = isset($program_food->day1) ? json_decode($program_food->day1, true) : [];
+$day2_values = isset($program_food->day2) ? json_decode($program_food->day2, true) : [];
+$day3_values = isset($program_food->day3) ? json_decode($program_food->day3, true) : [];
+$day4_values = isset($program_food->day4) ? json_decode($program_food->day4, true) : [];
+$day5_values = isset($program_food->day5) ? json_decode($program_food->day5, true) : [];
+$day6_values = isset($program_food->day6) ? json_decode($program_food->day6, true) : [];
+$day7_values = isset($program_food->day7) ? json_decode($program_food->day7, true) : [];
+$diets_data_day1 = [];
+
+foreach ($day1_values as $diet_id) {
+    // Fetch data for each ID from the "diets" table
+    $diet_data = get_diet_per_id($connect, $diet_id);
+    
+    // Add the fetched data to the array
+    $diets_data_day1[] = $diet_data;
+}
+
+$diets_data_day2 = [];
+foreach ($day2_values as $diet_id) {
+    // Fetch data for each ID from the "diets" table
+    $diet_data = get_diet_per_id($connect, $diet_id);
+    
+    // Add the fetched data to the array
+    $diets_data_day2[] = $diet_data;
+}
+$diets_data_day3 = [];
+foreach ($day2_values as $diet_id) {
+    // Fetch data for each ID from the "diets" table
+    $diet_data = get_diet_per_id($connect, $diet_id);
+    
+    // Add the fetched data to the array
+    $diets_data_day3[] = $diet_data;
+}
+
+$diets_data_day4 = [];
+foreach ($day4_values as $diet_id) {
+    // Fetch data for each ID from the "diets" table
+    $diet_data = get_diet_per_id($connect, $diet_id);
+    
+    // Add the fetched data to the array
+    $diets_data_day4[] = $diet_data;
+}
+
+
+$diets_data_day5 = [];
+foreach ($day5_values as $diet_id) {
+    // Fetch data for each ID from the "diets" table
+    $diet_data = get_diet_per_id($connect, $diet_id);
+    
+    // Add the fetched data to the array
+    $diets_data_day5[] = $diet_data;
+}
+$diets_data_day6 = [];
+foreach ($day6_values as $diet_id) {
+    // Fetch data for each ID from the "diets" table
+    $diet_data = get_diet_per_id($connect, $diet_id);
+    
+    // Add the fetched data to the array
+    $diets_data_day6[] = $diet_data;
+}
+$diets_data_day7 = [];
+foreach ($day7_values as $diet_id) {
+    // Fetch data for each ID from the "diets" table
+    $diet_data = get_diet_per_id($connect, $diet_id);
+    
+    // Add the fetched data to the array
+    $diets_data_day7[] = $diet_data;
+}
+$program["diets_data_day1"]=$diets_data_day1;
+$program["diets_data_day2"]=$diets_data_day2;
+$program["diets_data_day3"]=$diets_data_day3;
+$program["diets_data_day4"]=$diets_data_day4;
+$program["diets_data_day5"]=$diets_data_day5;
+$program["diets_data_day6"]=$diets_data_day6;
+$program["diets_data_day7"]=$diets_data_day7;
+return $program;
+
+    }else {
+        return false;
+    }
+}
+
 
 function number_workouts($connect)
 {
