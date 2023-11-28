56e73ecbb875ec14d05dbd135c6cf0b8fd51f6eb
Mhmad-jamal <110853406+Mhmad-jamal@users.noreply.github.com>
2023-11-28 21:56:18 +0300

    update

--- controller/need_update.php
+++ controller/need_update.php
@@ -1,8 +1,6 @@
 <?php
-
 require '../admin/config.php';
 require '../admin/functions.php';
-
 
 $response = array("status" => 0, "message" => "", "data" => null);
 $conn = new mysqli($database['host'], $database['user'], $database['pass'], $database['db']);
@@ -14,29 +12,27 @@ if ($conn->connect_error) {
 if (isset($_POST["user_id"])) {
     $user_id = $_POST["user_id"]; 
 
-    // Calculate the date one month ago from today
-    $oneMonthAgo = date("Y-m-d", strtotime("-1 month"));
+    // Calculate the date one week ago from today
+    $oneWeekAgo = date("Y-m-d", strtotime("-1 week"));
 
-    SELECT * 
-    FROM `users_goal` 
-    WHERE `user_id` = '$user_id' 
-          AND `created_at` >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK);
-        
+    $query = "SELECT * FROM `users_goal` WHERE `user_id` = '$user_id' AND `created_at` >= '$oneWeekAgo'";
+    
     $user_data = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($user_data) > 0) {
         while ($row = mysqli_fetch_assoc($user_data)) {
             // Process the retrieved records here
             $response["status"] = 200;
-            $response["message"] = "user Need update goal!";
+            $response["message"] = "user needs to update goal!";
         }
     } else {
         $response["status"] = 201;
         $response["message"] = "no need to update goal!";
     }
-}else{
+} else {
     $response["status"] = 404;
-    $response["message"] = "user not Found";
+    $response["message"] = "user not found";
 }
-echo json_encode($response);
 
+echo json_encode($response);
+?>
