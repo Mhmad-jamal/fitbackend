49721f57339662341c58488976f1ad9b4a348c38
Mhmad-jamal <110853406+Mhmad-jamal@users.noreply.github.com>
2023-11-29 23:50:01 +0300

    update

--- controller/updateUserGoal.php
+++ controller/updateUserGoal.php
@@ -20,8 +20,7 @@ if (isset($_POST["user_id"])) {
         $user_data = $sentence->fetch_assoc();
         $user_old_goal = json_decode($user_data["user_goal"], true);
             $user_goal=json_decode($user_goal);
-            var_dump($user_goal);
-            die();
+            
         // Check if JSON decoding is successful
         if (is_array($user_old_goal)) {
             foreach ($user_goal as $new_value) {
