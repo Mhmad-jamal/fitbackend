de61c2915f33177f46a9318e9090d734d7e19b02
Mhmad-jamal <110853406+Mhmad-jamal@users.noreply.github.com>
2023-11-29 23:39:40 +0300

    update

--- controller/updateUserGoal.php
+++ controller/updateUserGoal.php
@@ -3,23 +3,24 @@
 require '../admin/config.php';
 require '../admin/functions.php';
 
-
 $response = array("status" => 0, "message" => "", "data" => null);
 $conn = new mysqli($database['host'], $database['user'], $database['pass'], $database['db']);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 if (isset($_POST["user_id"])) {
-    $user_id = $_POST["user_id"]; 
+    $user_id = $_POST["user_id"];
     $user_goal = $_POST["user_goal"];
-    $sentence = $conn->prepare("SELECT * FROM `users_goal` WHERE user_id = :user_id");
-    $sentence->bindParam(':user_id', $user_id);
+    
+    $sentence = $conn->prepare("SELECT * FROM `users_goal` WHERE user_id = ?");
+    $sentence->bind_param('s', $user_id);
     $sentence->execute();
     
-    if ($sentence->rowCount() > 0) {
+    if ($sentence->num_rows > 0) {
         // User exists, fetch and update the data
-        $user_data = $sentence->fetch(PDO::FETCH_ASSOC);
+        $user_data = $sentence->fetch_assoc();
         $user_old_goal = json_decode($user_data["user_goal"], true); // true to get an associative array
     
         // Update existing values
@@ -33,24 +34,29 @@ if (isset($_POST["user_id"])) {
                 $user_old_goal[] = $new_value;
             }
         }
-    }
-    $query = "UPDATE `users_goal` SET `user_goal` = '$user_goal', `created_at` = NOW() WHERE `users_goal`.`user_id` LIKE '$user_id';";
-    
-    $user_data = mysqli_query($conn, $query);
-
-    if ($user_data) {
-        // The query was successful
-        $response["status"] = 200;
-        $response["message"] = "User goal updated!";
+        
+        // Update the user's record in the database using prepared statement
+        $updateQuery = "UPDATE `users_goal` SET `user_goal` = ?, `created_at` = NOW() WHERE `user_id` = ?";
+        $updateStatement = $conn->prepare($updateQuery);
+        $updateStatement->bind_param('ss', json_encode($user_old_goal), $user_id);
+        $updateStatement->execute();
+        
+        if ($updateStatement->affected_rows > 0) {
+            $response["status"] = 200;
+            $response["message"] = "User goal updated!";
+        } else {
+            $response["status"] = 201;
+            $response["message"] = "No need to update goal or an error occurred.";
+        }
     } else {
-        // The query failed
-        $response["status"] = 201;
-        $response["message"] = "No need to update goal or an error occurred.";
+        $response["status"] = 404;
+        $response["message"] = "User not found";
     }
 } else {
     $response["status"] = 404;
-    $response["message"] = "User not found";
+    $response["message"] = "User ID not provided";
 }
 
 echo json_encode($response);
 
+?>
