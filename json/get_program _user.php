c6b2e89511e9859a618d535619fdb264e87b0090
Mhmad-jamal <110853406+Mhmad-jamal@users.noreply.github.com>
2023-12-02 18:55:37 +0300

    update

--- json/get_program _user.php
+++ json/get_program _user.php
@@ -41,8 +41,7 @@ $qResults = $sentence->fetchAll(PDO::FETCH_ASSOC);
 $new_arr = array();
 foreach ($qResults as $key => $value) {
     $program = get_food_program_by_id_mobile($connect, $value['du_diet']);
-  echo json_encode($program);
-  die();
+  
     if ($program !== false) {
         array_push($new_arr, $program);
     }
