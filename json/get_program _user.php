1f7943d5ec79ec84eec79e8fe1e5c5f099c10ec9
Mhmad-jamal <110853406+Mhmad-jamal@users.noreply.github.com>
2023-12-02 17:11:51 +0300

    update

--- json/get_program _user.php
+++ json/get_program _user.php
@@ -1,53 +1,47 @@
 <?php
 
 $page = 1;
-if(!empty($_GET['page'])) {
+if (!empty($_GET['page'])) {
     $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
-    if(false === $page) {
+    if ($page === false || $page < 1) {
         $page = 1;
     }
 }
 
 $limit = 10;
-if(!empty($_GET['limit'])) {
+if (!empty($_GET['limit'])) {
     $limit = filter_input(INPUT_GET, 'limit', FILTER_VALIDATE_INT);
+    if ($limit === false || $limit < 1) {
+        $limit = 10;
+    }
 }
 
 $offset = ($page - 1) * $limit;
 
 header('Content-Type: application/json');
-header("access-control-allow-origin: *");
+header("Access-Control-Allow-Origin: *");
 
 require './app_core.php';
 
-$sqlQuery = "SELECT * FROM program_food ";
+$sqlQuery = "SELECT * FROM program_food";
 
+if (getParamsUser()) {
+    $sqlQuery .= " WHERE program_food.id IN (SELECT diets_users.du_diet FROM diets_users WHERE diets_users.du_user = '" . getParamsUser() . "')";
+}
 
+$sqlQuery .= " ORDER BY program_food.id DESC";
 
-	if(getParamsUser()){
+if (isset($_GET['page']) && !empty($_GET['page'])) {
+    $sqlQuery .= " LIMIT " . $offset . "," . $limit;
+}
 
-        $sqlQuery .= " AND program_food.id IN (SELECT diets_users.du_diet FROM diets_users WHERE diets_users.du_user = '".getParamsUser()."')";
-	}
+if (isset($_GET['limit']) && !empty($_GET['limit']) && !isset($_GET['page'])) {
+    $sqlQuery .= " LIMIT " . $limit;
+}
 
-    $sqlQuery .= " ORDER BY diets.diet_id DESC";
+$sentence = $connect->prepare($sqlQuery);
+$sentence->execute();
+$qResults = $sentence->fetchAll(PDO::FETCH_ASSOC);
 
-    if(isset($_GET['page']) && !empty($_GET['page'])) {
-        $sqlQuery .= " LIMIT ".$offset.",".$limit;
-    }
-
-    if(isset($_GET['limit']) && !empty($_GET['limit']) && !isset($_GET['page'])) {
-        $sqlQuery .= " LIMIT ".$limit;
-    }
-    
-    $sentence = $connect->prepare($sqlQuery);
-
-    $sentence->execute();
-
-    $qResults = $sentence->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-
-	print json_encode($qResults, JSON_NUMERIC_CHECK);
-
+echo json_encode($qResults, JSON_NUMERIC_CHECK);
 ?>
