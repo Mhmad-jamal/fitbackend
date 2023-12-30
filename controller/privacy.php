d27b4048c15e791978885a7811b3053d8899edd9
Mhmad-jamal <110853406+Mhmad-jamal@users.noreply.github.com>
2023-12-31 00:44:02 +0300

    add privacy

---
+++ controller/privacy.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Privacy Policy for Mohannad Theeb مهند ذيب Gym App</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Privacy Policy for Mohannad Theeb مهند ذيب Gym App</h1>
+        <p>Last Updated: 10/25/2023</p>
+    </header>
+
+    <section>
+        <h2>Information We Collect</h2>
+        <p>When you use Mohannad Theeb مهند ذيب Gym App, we collect the following types of information:</p>
+        <ol>
+            <li><strong>Account Information:</strong> When you create an account, we collect your name, email address, and other relevant details.</li>
+            <li><strong>Fitness Data:</strong> We may collect and process data related to your fitness activities, such as workout routines, exercise history, and performance metrics.</li>
+            <li><strong>Device Information:</strong> We collect information about the device you use to access our application, including device type, operating system, and unique device identifiers.</li>
+        </ol>
+    </section>
+
+    <!-- Add more sections for "How We Use Your Information," "Data Sharing," "Data Security," "Your Choices," "Changes to This Policy," and "Contact Us" -->
+
+    <section>
+        <h2>Data Security</h2>
+        <p>We implement reasonable security measures to protect your personal information from unauthorized access, disclosure, alteration, and destruction.</p>
+    </section>
+
+    <section>
+        <h2>Your Choices</h2>
+        <p>You can manage your account settings and communication preferences through the Mohannad Theeb مهند ذيب Gym App. You can also request access to, correction of, or deletion of your personal information by contacting us at info@mohannad-theeb.com.</p>
+    </section>
+
+    <section>
+        <h2>Changes to This Policy</h2>
+        <p>We may update this Privacy Policy from time to time. The latest version will be posted within the Mohannad Theeb مهند ذيب Gym App, and your continued use constitutes acceptance of the updated policy.</p>
+    </section>
+
+    <section>
+        <h2>Contact Us</h2>
+        <p>If you have any questions or concerns about this Privacy Policy, please contact us at info@mohannad-theeb.com.</p>
+    </section>
+
+ 
+
+</body>
+
+</html>
